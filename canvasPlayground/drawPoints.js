ca97a1d88e3dab6633fca29b9a936f0f2c6d1989
zenghawtin <1448593512@qq.com>
2024-06-22 20:44:39 +0800

    feat: input event handler

--- canvasPlayground/drawPoints.js
+++ canvasPlayground/drawPoints.js
@@ -1,21 +1,27 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const drawEle = document.getElementById("draw");
-const inputEle = document.getElementById("point");
+const textarea = document.getElementById("point");
+const canvasInfo = document.getElementById("canvasInfo");
 
 window.onload = function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  canvasInfo.innerText = `width: ${window.innerWidth}, height: ${window.innerHeight}`;
 };
 
-drawEle.addEventListener("click", () => {
+textarea.addEventListener("input", () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  var string = inputEle.value === "" ? "0" : inputEle.value;
+  var string = textarea.value === "" ? "0" : textarea.value;
   eval("var points=" + string);
 
   points.forEach?.((point) => {
+    ctx.save();
+    ctx.beginPath();
     ctx.fillStyle = "red";
     ctx.arc(point.x, point.y, 10, 0, Math.PI * 2);
+    ctx.closePath();
     ctx.fill();
+    ctx.restore();
   });
 });
